f62a394716c4d34c81ee910370a26b3a98ee1e79
Moon <Moon@DESKTOP-USC4RQ7>
2023-10-24 12:27:13 +0900

    내용이 변경됬습니다.

--- PublicGitTest/src/main/java/mmsshh/MMSSHH.java
+++ PublicGitTest/src/main/java/mmsshh/MMSSHH.java
@@ -4,8 +4,8 @@ public class MMSSHH {
 	
 public static void main(String[] args) {
 		
-		System.out.println("연습용: 팀원 미스터 문이 작업한 것임.");
-
+		System.out.println("테스트용: 팀원 미스터 문이 작업한 것임.");
+		System.out.println("변경된거임!.");
 
 	}
 
