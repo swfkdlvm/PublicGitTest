51a70a0653a878a8e5a01e447a99e4396faeca9e
X390 <X390@DESKTOP-5MPH0TH>
2023-10-24 12:20:24 +0900

    팀장 전용 브랜치인 kimjy 브랜치에서 클래스 생성함

---
+++ PublicGitTest/src/main/java/teamleader/test/TeamLeaderTest.java
@@ -0,0 +1,12 @@
+package teamleader.test;
+
+public class TeamLeaderTest {
+
+	public static void main(String[] args) {
+		// 팀장 전용 브랜치인 kimjy 브랜치에서 생성한 클래스임.
+		
+		System.out.println("팀장 전용 브랜치에서 생성한 클래스임");
+		
+	}
+
+}
